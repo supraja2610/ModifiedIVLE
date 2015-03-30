5f8e4cf4f1d17f2ac457bf6066f6a3d3c1624a9b
supraja2610 <suprajabhavanisekhar@gmail.com>
2015-03-30 11:03:01 +0800

    admin view

---
+++ Frontend/js/adminConsole.js
@@ -0,0 +1,7 @@
+$(function() { 
+    $( "#create-mod" ).click(function() {
+       // $("div#mod-create-form").show();
+       $("#mod-create-form").removeClass('hide');
+        console.log("hi");
+    });
+});
