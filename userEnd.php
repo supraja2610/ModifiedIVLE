be2f29c56952bcacc7a21fab5289be8b95eb80ba
supraja2610 <suprajabhavanisekhar@gmail.com>
2015-04-06 11:56:52 +0800

    all working

--- userEnd.php
+++ userEnd.php
@@ -1,11 +1,35 @@
 <?php
+
+
+session_start();
+$_SESSION['time'] = date('Y-m-d h:i:s');
+$time = $_SESSION['time'];
+
+if (empty($_SESSION['visitcount'])) {
+  $_SESSION['visitcount'] = 1;
+  $_SESSION['visittime'] = date('Y-m-d h:i:s');
+  $time = '2010-05-24 21:00:00';
+}
+else {
+  $_SESSION['visitcount']++;
+  $time = $_SESSION['visittime'];
+  $_SESSION['visittime'] = date('Y-m-d h:i:s');
+}
+
+
 require_once '../../config.php'; // your PHP script(s) can access this, but the rest cannot
 $db = new mysqli(db_host, db_uid, db_pwd, db_name);
 if ($db->connect_errno) // are we connected properly?
   exit("Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error);
 
-if($_POST['cmd'] == "load")
-        is_best_score();
+if($_POST['cmd'] == "load"){
+  
+  $ret1 = is_best_score();
+  $ret2 = DisplayNewsContent($time);
+  $retVal = array('lesson'=>$ret1, 'news'=>$ret2);
+  echo json_encode($retVal);
+}
+        
 
 if($_POST['cmd'] == "getweek")
         getWeek();
@@ -27,10 +51,36 @@ function is_best_score() {
    		// $row = mysqli_fetch_row($res);
    		// $results = $db->fetch_all($res,MYSQLI_ASSOC);
    		$n_col = $db->field_count;
-   		echo json_encode($rows);
+   		return json_encode($rows);
    }
-    $db->close();
+    
 }
+
+function DisplayNewsContent($timestamp) {  
+  global $db; // refer to the global variable 'db'
+  $table_name = "news";
+  $query = "SELECT * FROM " . $table_name. " WHERE newsDate > '$timestamp'";
+  
+  $res = $db->query($query); // just like this, MySQL command line to PHP command
+  if (!$res){ // is there any error?
+    exit("MySQL reports " . $db->error);
+  }
+  
+  
+  $checkempty = $res->num_rows;
+  
+  if($checkempty != 0){
+
+  $rows = resultToArray($res);
+  $res->free();
+  return json_encode($rows);
+    
+  }
+ 
+
+}
+
+
 function resultToArray($result) {
     $rows = array();
     while($row = $result->fetch_assoc()) {
@@ -52,6 +102,5 @@ function getWeek(){
    	$row = mysqli_fetch_row($res);
    	echo $row[1];
    }
-    $db->close();
 }
 ?>
